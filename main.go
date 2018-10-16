b7b94e5e1c37219c6c96c477ebb8f821634bd2d0
difoul <foudil.bendjabeur@outlook.com>
2018-10-16 23:00:42 +0200

    Basic auth - list of GET and POST responses

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/difoul/rest-api-mock/helpers"
@@ -17,7 +16,6 @@ var resp_c string
 func main() {
 	fileFlag := flag.String("resp-file", "", "The path to the configuration file that contains the response")
 	flag.Parse()
-	fmt.Println(*fileFlag)
 	if *fileFlag == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -27,24 +25,15 @@ func main() {
 		os.Stderr.WriteString(fmt.Sprintf("The file [%s] does not exist!\n", resp_file))
 		os.Exit(1)
 	}
-	content := helpers.ReadFile("conf/response.json")
+	content := helpers.ReadFile(resp_file)
 	//fmt.Println(content)
-	var resp models.HttpGetMock
-	err := json.Unmarshal(content, &resp)
+	var mock models.HttpMock
+	err := json.Unmarshal(content, &mock)
 	if err != nil {
 		os.Stderr.WriteString(err.Error())
 		os.Exit(1)
 	}
-	resp_c = resp.Response
-	fmt.Println(resp_c)
 
 	router := gin.Default()
-	router.GET(resp.Url, GetResp)
-	router.Run(fmt.Sprintf(":%d", resp.Port)) // listen and serve on 0.0.0.0:8080
-}
-
-func GetResp(c *gin.Context) {
-	//c.JSON(http.StatusOK, gin.H{"message": resp_c})
-	//c.JSON(http.StatusOK, resp_c)
-	c.Data(http.StatusOK, "application/json; charset=utf-8", []byte(resp_c))
+	mock.RegisterEndpoints(router)
 }
--- models/mock.go
+++ models/mock.go
@@ -1,7 +1,42 @@
 package models
 
-type HttpGetMock struct {
-	Port     int    `json:"port"`
-	Url      string `json:"url"`
-	Response string `json:"response"`
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HttpMock struct {
+	Port     int        `json:"port"`
+	Username string     `json:"username"`
+	Password string     `json:"password"`
+	Get      []Response `json:"get"`
+	Post     []Response `json:"post"`
+}
+
+type Response struct {
+	Url  string `json:"url"`
+	Resp string `json:"response"`
+}
+
+func (m *HttpMock) RegisterEndpoints(engine *gin.Engine) {
+	router := engine.Group("/")
+	if len(m.Username) > 0 && len(m.Password) > 0 {
+		router = engine.Group("/", gin.BasicAuth(gin.Accounts{m.Username: m.Password}))
+	}
+
+	for _, g := range m.Get {
+		router.GET(g.Url, func(c *gin.Context) {
+			c.Data(http.StatusOK, "application/json; charset=utf-8", []byte(g.Resp))
+		})
+	}
+
+	for _, p := range m.Post {
+		router.POST(p.Url, func(c *gin.Context) {
+			c.Data(http.StatusOK, "application/json; charset=utf-8", []byte(p.Resp))
+		})
+	}
+
+	engine.Run(fmt.Sprintf(":%d", m.Port))
 }
