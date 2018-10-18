826a20b1b899f8d0942c061123ac6adc03f6861b
difoul <foudil.bendjabeur@outlook.com>
2018-10-18 22:21:39 +0200

    Add PUT, PATCH, DELETE handlers

--- models/mock.go
+++ models/mock.go
@@ -12,6 +12,9 @@ type HttpMock struct {
 	Password string     `json:"password"`
 	Get      []Response `json:"get"`
 	Post     []Response `json:"post"`
+	Put      []Response `json:"put"`
+	Patch    []Response `json:"patch"`
+	Delete   []Response `json:"delete"`
 }
 
 type Response struct {
@@ -33,15 +36,30 @@ func (m *HttpMock) RegisterEndpoints(engine *gin.Engine) {
 		router = engine.Group("/", gin.BasicAuth(gin.Accounts{m.Username: m.Password}))
 	}
 
-	for _, g := range m.Get {
+	for _, r := range m.Get {
 		// https://stackoverflow.com/questions/48826460/using-pointers-in-a-for-loop-golang
-		get := g
-		router.GET(get.Url, get.HandleResponse)
+		resp := r
+		router.GET(resp.Url, resp.HandleResponse)
 	}
 
-	for _, p := range m.Post {
-		post := p
-		router.POST(p.Url, post.HandleResponse)
+	for _, r := range m.Post {
+		resp := r
+		router.POST(resp.Url, resp.HandleResponse)
+	}
+
+	for _, r := range m.Put {
+		resp := r
+		router.PUT(resp.Url, resp.HandleResponse)
+	}
+
+	for _, r := range m.Patch {
+		resp := r
+		router.PATCH(resp.Url, resp.HandleResponse)
+	}
+
+	for _, r := range m.Delete {
+		resp := r
+		router.DELETE(resp.Url, resp.HandleResponse)
 	}
 
 	engine.Run(fmt.Sprintf(":%d", m.Port))
